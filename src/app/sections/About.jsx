be6b706c3dc326b63b8463d6946165a2698b37df
Yaw Amo Mensah <amomensah300@gmail.com>
2024-10-05 14:25:40 +0100

    added additional grid items

--- src/app/sections/About.jsx
+++ src/app/sections/About.jsx
@@ -1,10 +1,22 @@
 'use client';
 
-import React from 'react'
+import React, { useState} from 'react'
 import Globe from 'react-globe.gl'
 import Button from '../components/Button';
 
 const About = () => {
+  const [hasCopied, setHasCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText('amomensah300@gmail.com');
+
+    setHasCopied(true);
+
+    setTimeout(() => {
+        setHasCopied(false);
+    }, 2000);
+  };
+
   return (
     <section className='c-space my-20'>
         <div className='grid xl:grid-cols-3
@@ -53,6 +65,31 @@ const About = () => {
                 </div>
             </div>
             <div>
+                <div className='grid-container'>
+                    <img src='/assets/grid3.png' alt='grid-3'
+                        className='w-full sm:h-[276px] h-fit object-contain' />
+                        <div>
+                            <p className='grid-headtext'>My Passion for Coding</p>
+                            <p className='grid-subtext'>I love solving problems and building solutions through programming.
+                            Coding is not just my profession - it is my passion.</p>
+                        </div>
+                </div>
+            </div>
+            <div className='xl:col-span-1 xl:row-span-2'>
+                <div className='grid-container'>
+                    <img src='/assets/grid4/png' alt='grid-4' className='w-full md:h-[126px] sm:h-[276px] 
+                    h-fit object-cover sm:object-top'/>
+                    <div className='space-y-2'>
+                        <p className='grid-subtext text-center'>Contact Me</p>
+                        <div className='copy-container' onClick={handleCopy}>
+                            <img src={hasCopied ? '/assets/tick.svg' : '/assets/copy.svg'} alt='copy' />
+                            <p className='lg:text-2xl md:text-xl font-medium text-gray_gradient
+                            text-white'>amomensah300@gmail.com</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
         
     </section>
