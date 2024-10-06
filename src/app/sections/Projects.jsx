1a5917dc89b3475a587c63e822695b129c5136c9
amomensah675 <amomensah675@gmail.com>
2024-10-06 04:32:11 +0000

    added Canvas

--- src/app/sections/Projects.jsx
+++ src/app/sections/Projects.jsx
@@ -1,7 +1,14 @@
 'use client';
 
+import { Suspense, useState } from "react";
 import { myProjects } from "@/constants";
-import { useState } from "react";
+import dynamic from 'next/dynamic';
+
+import { Center, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
+import CanvasLoader from "../components/CanvasLoader";
+import DemoComputer from "../components/DemoComputer";
 
 const projectCount = myProjects.length;
 
@@ -75,7 +82,19 @@ const Projects = () => {
             </div>
 
             <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
+                        <Canvas>
+                            <ambientLight intensity={Math.PI} />
+                            <directionalLight position={[10, 10, 5]} />
 
+                            <Center>
+                                <Suspense fallback={<CanvasLoader />}>
+                                    <group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
+                                        <DemoComputer />
+                                    </group>
+                                </Suspense>
+                            </Center>
+                            <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false} />
+                        </Canvas>
             </div>
         </div>
     </section>
